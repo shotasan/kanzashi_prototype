0c3c09f28ee1730a3287ea6680b1791d24b9c2db
shotasan <wondermoai@gmail.com>
2019-04-19 20:51:17 +0900

    ログイン後アカウント編集後の遷移先を指定するためコントローラに追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,16 @@ class ApplicationController < ActionController::Base
   # deviseコントローラーにストロングパラメータを追加する
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ログイン後、blogs/indexに移動する
+  def after_sign_in_path_for(resource)
+    beans_path
+  end
+
   protected
   def configure_permitted_parameters
     # サインアップ時にnameのストロングパラメータを追加
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    # アカウント編集の時にnameとprofileのストロングパラメータを追加
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+    # アカウント編集の時にnameとprofileとiconのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :icon])
   end
 end
