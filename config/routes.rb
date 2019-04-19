1203e39c8895b1dff71de791bb8a7493e51407fd
shotasan <wondermoai@gmail.com>
2019-04-19 20:45:21 +0900

    deviseのルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
+
+  # ログイン、アカウント編集後、任意のページに推移させるための記述
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 end
