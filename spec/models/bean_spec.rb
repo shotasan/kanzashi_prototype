6d50c4f48486ca0ffe33c11ede35bd0d45419f2d
shotasan <wondermoai@gmail.com>
2019-04-20 21:40:48 +0900

    Beanモデルのモデルspecを記載

--- spec/models/bean_spec.rb
+++ spec/models/bean_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Bean, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:first_bean){ FactoryBot.create(:bean) }
+
+  context "バリデーションのテスト" do
+
+    it "nameが無ければ無効な状態であること" do
+      first_bean.name = nil
+      expect(first_bean.valid?).to be false
+    end
+
+    it "nameが30文字以内なら有効な状態であること" do
+      first_bean.name = "a" * 30
+      expect(first_bean.valid?).to be true
+    end
+
+    it "nameが30文字より多いなら無効な状態であること" do
+      first_bean.name = "a" * 31
+      expect(first_bean.valid?).to be false
+    end
+
+    it "descriptionが1000文字以内なら有効な状態であること" do
+      first_bean.description = "a" * 1000
+      expect(first_bean.valid?).to be true
+    end
+
+    it "descriptionが1000文字より多いなら無効な状態であること" do
+      first_bean.description = "a" * 1001
+      expect(first_bean.valid?).to be false
+    end
+  end
 end
