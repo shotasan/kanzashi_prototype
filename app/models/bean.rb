0fbbe68c61c28c9a5f323e12209ca9f72dbebaa8
shotasan <wondermoai@gmail.com>
2019-04-20 21:53:22 +0900

    Beanモデルのモデルspecを追記

--- app/models/bean.rb
+++ app/models/bean.rb
@@ -2,6 +2,11 @@ class Bean < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, length: { maximum: 30 }
+  validates :country, length: { maximum: 30 }
+  validates :plantation, length: { maximum: 30 }
+  validates :roasted, length: { maximum: 30 }
+  validates :grind, length: { maximum: 30 }
+  validates :store, length: { maximum: 30 }
   validates :description, length: { maximum: 1000 }
 
   mount_uploader :icon, IconUploader
