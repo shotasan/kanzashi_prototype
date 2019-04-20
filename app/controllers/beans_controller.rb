53643f58219294e1ff5d9a50602a7fe6dfe40359
shotasan <wondermoai@gmail.com>
2019-04-20 16:31:03 +0900

    beansコントローラを作成。beanのルーティングを作成。

---
+++ app/controllers/beans_controller.rb
@@ -0,0 +1,28 @@
+class BeansController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def bean_params
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # ログイン、アカウント編集後、任意のページに推移させるための記述
+
+  # ログイン、アカウント編集後、任意のページに推移させるための記述（一時的に削除中、あとで追加）
   devise_for :users
 
   resources :users, only: [:show]
+  resources :beans
 end
