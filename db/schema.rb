e8928d35c527d38fba3d73259db8d27b59c2c9d1
shotasan <wondermoai@gmail.com>
2019-04-22 01:44:12 +0900

    tasteテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_20_053521) do
+ActiveRecord::Schema.define(version: 2019_04_21_064033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,20 @@ ActiveRecord::Schema.define(version: 2019_04_20_053521) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_beans_on_user_id"
+  end
+
+  create_table "tastes", force: :cascade do |t|
+    t.text "impression", default: "", null: false
+    t.integer "bitter", null: false
+    t.integer "acidity", null: false
+    t.integer "rich", null: false
+    t.integer "sweet", null: false
+    t.integer "aroma", null: false
+    t.string "tasteable_type"
+    t.bigint "tasteable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tasteable_type", "tasteable_id"], name: "index_tastes_on_tasteable_type_and_tasteable_id"
   end
 
   create_table "users", force: :cascade do |t|
---
+++ spec/factories/tastes.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :taste do
+    impression { "MyText" }
+    bitter { 1 }
+    acidity { 1 }
+    rich { 1 }
+    sweet { 1 }
+    aroma { 1 }
+  end
+end
