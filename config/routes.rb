927ddd4be6918ba0154c0400a9b5b2290bea9341
shotasan <wondermoai@gmail.com>
2019-04-22 18:03:43 +0900

    rootを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root "beans#index"
 
-  get 'favorite_beans/index'
-  get 'favorite_beans/create'
-  get 'favorite_beans/destroy'
   # ログイン、アカウント編集後、任意のページに推移させるための記述（一時的に削除中、あとで追加）
-  devise_for :users
+  devise_for :users, controllers: {
+      registrations: 'users/registrations'
+  }
 
   resources :users, only: %i[show]
   resources :beans
