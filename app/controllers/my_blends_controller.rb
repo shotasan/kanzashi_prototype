e0567a56636dc61bd266395c270afaee43b3032c
shotasan <wondermoai@gmail.com>
2019-04-22 21:25:16 +0900

    my_blendsの新規作成機能を実装

--- app/controllers/my_blends_controller.rb
+++ app/controllers/my_blends_controller.rb
@@ -3,13 +3,18 @@ class MyBlendsController < ApplicationController
     @blends = MyBlend.all
   end
 
-  def index
-  end
-
   def new
+    @blend = MyBlend.new
+    @blend.build_taste
   end
 
   def create
+    @blend = current_user.my_blends.build(blend_params)
+    if @blend.save
+      redirect_to my_blends_path, notice: "#{@blend.name}の登録に成功しました。"
+    else
+      render :new
+    end
   end
 
   def show
@@ -22,5 +27,12 @@ class MyBlendsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def blend_params
+    params.require(:my_blend).permit(:name, :choice1, :choice2, :choice3, :amount1, :amount2, :amount3, :description,
+                                    taste_attributes: [:bitter, :acidity, :rich, :sweet, :aroma, :impression])
   end
 end
--- app/models/my_blend.rb
+++ app/models/my_blend.rb
@@ -1,5 +1,5 @@
 class MyBlend < ApplicationRecord
   belongs_to :user
-  # has_one :taste, as: :tasteable, dependent: :destroy
-
+  has_one :taste, as: :tasteable, dependent: :destroy
+  accepts_nested_attributes_for :taste, allow_destroy: true
 end
