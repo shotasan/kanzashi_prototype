fd39ebdbf4afaaf84e34c37a7f1df6a52a33dad9
shotasan <wondermoai@gmail.com>
2019-04-22 11:38:29 +0900

    Tasteモデルにバリデーションを設定

--- app/models/taste.rb
+++ app/models/taste.rb
@@ -1,3 +1,12 @@
 class Taste < ApplicationRecord
   belongs_to :tasteable, polymorphic: true
+
+  validates :impression, presence: true, length: { maximum: 1000 }
+  validates :bitter, :acidity, :rich, :sweet, :aroma,
+             presence:  true,
+             numericality: {
+                 only_integer: true,
+                 greater_than_or_equal_to: 1,
+                 less_than_or_equal_to: 5
+             }
 end
