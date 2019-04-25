39c3e913dabf6aa10a6f198098c479bebb67e880
shotasan <wondermoai@gmail.com>
2019-04-25 17:46:55 +0900

    chart.jsを使用してBeanとMyBlendの詳細画面にチャート機能を実装

--- app/models/taste.rb
+++ app/models/taste.rb
@@ -4,4 +4,12 @@ class Taste < ApplicationRecord
   validates :impression, presence: true, length: { maximum: 1000 }
   validates :bitter, :acidity, :rich, :sweet, :aroma,
              presence:  true, inclusion: { in: [1, 2, 3, 4, 5]}
+
+  def taste_index
+    [self.bitter, self.acidity, self.rich, self.sweet, self.aroma]
+  end
+
+  def taste_labels
+    %w(苦味 酸味 コク 甘み 香り)
+  end
 end
