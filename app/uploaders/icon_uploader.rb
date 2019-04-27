3f3598c66b474f7e05c5067c5a24c57d3919381b
shotasan <wondermoai@gmail.com>
2019-04-28 03:24:08 +0900

    ログイン、サインインページのデザインを修正

--- app/uploaders/icon_uploader.rb
+++ app/uploaders/icon_uploader.rb
@@ -7,7 +7,7 @@ class IconUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
-  process :resize_to_limit => [100, 100] # 画像サイズの調整
+  process :resize_to_limit => [300, 300] # 画像サイズの調整
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
