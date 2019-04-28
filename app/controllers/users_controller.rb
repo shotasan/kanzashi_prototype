20804c50a3b7d72998bcab004f8787423af24cfe
shotasan <wondermoai@gmail.com>
2019-04-28 18:08:26 +0900

    マイページのお気に入りから、各詳細画面に遷移できるように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @bean_favorites = current_user.fav_beans.includes(:taste)
-    @blend_favorites = current_user.fav_blends.includes(:taste)
+    @bean_favorites = current_user.fav_beans.includes(:user, :taste)
+    @blend_favorites = current_user.fav_blends.includes(:user, :taste)
   end
 end
