666eeda19113c76f271dfef7eb776b44699db482
shotasan <wondermoai@gmail.com>
2019-04-29 01:30:02 +0900

    application.jsの記載を修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require jquery/dist/jquery.js
 //= require popper
 //= require bootstrap-sprockets
 //= require bootstrap
 //= require Chart.min
+//= require rails-ujs
+//= require turbolinks
+//= require node_modules/jquery/dist/jquery.js
 //= require_tree .
