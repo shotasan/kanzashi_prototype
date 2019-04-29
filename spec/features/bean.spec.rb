f8577d506f5bb7ac32c410ad801930fbfedb0489
shotasan <wondermoai@gmail.com>
2019-04-30 04:28:53 +0900

    bean.spec.rbを追記

--- spec/features/bean.spec.rb
+++ spec/features/bean.spec.rb
@@ -2,29 +2,26 @@ require 'rails_helper'
 
 RSpec.describe "ストレートコーヒー投稿機能", type: :feature do
 
-  let(:user_a) { FactoryBot.create(:user, name: "ユーザーA")}
-  let(:bean_a) { FactoryBot.catate(:bean, name: "豆A")}
-  let(:bean_b) { FactoryBot.catate(:bean, name: "豆B")}
-  let(:bean_c) { FactoryBot.catate(:bean, name: "豆C")}
-
+  user_a = User.create(name: "ユーザーA", email: "a@example.com", password: "password")
+  bean_a = user_a.beans.create(name: "豆A", price: 1_000)
+  bean_a.create_taste(impression: "tasteA", bitter: 1, acidity: 1, rich: 1, sweet: 1, aroma: 1)
 
   before do
     visit root_path
-    fill_in "メールアドレス", with: login_user.email
-    fill_in "パスワード", with: login_user.password
+    fill_in "メールアドレス", with: user_a.email
+    fill_in "パスワード", with: user_a.password
     click_on "Log in"
   end
 
   describe "一覧画面のテスト" do
     context "ユーザーAがログインしているとき" do
-      let(:login_user){ user_a }
 
       it "登録が最新の投稿が先に表示される(作成日時の降順）" do
         visit beans_path
         save_and_open_page
-        within ".table-body" do
-          task_titles = all(".task-title").map(&:text)
-          expect(task_titles).to eq %w(Aの3番目のタスク Aの2番目のタスク Aの最初のタスク)
+        within ".article" do
+          beans_name = all(".article").map(&:text)
+          expect(beans_name).to eq %w(豆C 豆B 豆A)
         end
       end
     end
