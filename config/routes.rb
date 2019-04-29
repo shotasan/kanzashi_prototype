06cde1e740eb03ccb77ae10617f24cce91045483
shotasan <wondermoai@gmail.com>
2019-04-29 10:41:08 +0900

    routes.rbを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_scope :user do
     root to: "devise/sessions#new"
+    get sign_out: "devise/sessions#destroy", as: "destroy_user_session"
   end
 
   devise_for :users, controllers: {
