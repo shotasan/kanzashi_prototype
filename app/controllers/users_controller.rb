1bbb2f0b2d2a8b3169d88f1229011b3e3656553a
shotasan <wondermoai@gmail.com>
2019-04-30 22:15:19 +0900

    ログイン後のメッセージの挙動を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+    flash = []
     @user = User.find(params[:id])
     @bean_favorites = @user.fav_beans.includes(:user, :taste)
     @blend_favorites = @user.fav_blends.includes(:user, :taste)
